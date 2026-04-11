6f0c5029ff2509c6c7e105354725d38b71b28022
Amelie Yu <amelie.m.yu@gmail.com>
2026-04-11 10:41:08 -0500

    install vuepython

---
+++ app/pages/pytest.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="flex flex-row w-full p-3 primary">
+    <div class="text-2xl">Vue Python</div>
+    <div class="flex justify-end flex-grow">
+      <py-status :py="py"></py-status>
+    </div>
+  </div>
+  <div class="container mx-auto">
+    <div class="p-8">
+      <py-code-block id="script" :py="py" :code="code"></py-code-block>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onBeforeMount } from 'vue';
+import { usePython } from "usepython";
+import { PyStatus, PyCodeBlock } from "vuepython";
+import "vuepython/style.css";
+import "highlight.js/styles/stackoverflow-light.css"
+
+const py = usePython()
+
+const code = `print('starting python script')
+a = 1
+b = 2
+print('finished python script')
+c = a + b
+# return value
+c`;
+
+async function init() {
+  await py.load();
+}
+
+onBeforeMount(() => init())
+</script>
