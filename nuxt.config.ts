ef6e20edb4c9dfbc8f5918210ecc1749540bea19
Amelie Yu <amelie.m.yu@gmail.com>
2026-04-11 10:02:06 -0500

    the ungambling

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
-      title: 'Alzheimer\'s Slot Machine',
+      title: 'Wavethinker',
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
