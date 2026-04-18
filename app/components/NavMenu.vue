694c618f030c56d7ea342757d44823d02f2c14a9
Amelie Yu <amelie.m.yu@gmail.com>
2026-04-18 15:24:09 -0500

    change arrow rotation when open/close

--- app/components/NavMenu.vue
+++ app/components/NavMenu.vue
@@ -11,9 +11,10 @@
           }"> 
           <div class="w-full flex flex-row justify-center">
             <div class="bg-primary h-10 w-16 flex flex-row justify-center p-2 text-white rounded-t-lg">
-                <svg viewBox="0 0 1024 1024" class="icon size-8" version="1.1" xmlns="http://www.w3.org/2000/svg" fill="#FFFFFF"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M903.232 256l56.768 50.432L512 768 64 306.432 120.768 256 512 659.072z" fill="#FFFFFF"></path></g></svg>
-            </div>
+                <svg v-if="!isMenuOpen" viewBox="0 0 1024 1024" class="icon size-8" version="1.1" xmlns="http://www.w3.org/2000/svg" fill="#FFFFFF" stroke="#FFFFFF"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M903.232 768l56.768-50.432L512 256l-448 461.568 56.768 50.432L512 364.928z" fill="#FFFFFF"></path></g></svg>
 
+                  <svg v-else viewBox="0 0 1024 1024" class="icon size-8" version="1.1" xmlns="http://www.w3.org/2000/svg" fill="#FFFFFF"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M903.232 256l56.768 50.432L512 768 64 306.432 120.768 256 512 659.072z" fill="#FFFFFF"></path></g></svg>
+            </div>
           </div>
           <div class="w-full bg-primary flex flex-row justify-center">
             <UNavigationMenu
